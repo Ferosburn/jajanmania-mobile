f21f2b846abd48d5ab9d1a5986e31801f7bfe107
Su'udi Khoirul Anam <udi.khoirul@gmail.com>
2023-10-14 17:09:56 +0700

    refactor(button): refactor font styling

--- ui/src/main/java/com/tokodizital/jajanmania/ui/components/buttons/BaseOutlinedButton.kt
+++ ui/src/main/java/com/tokodizital/jajanmania/ui/components/buttons/BaseOutlinedButton.kt
@@ -4,17 +4,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tokodizital.jajanmania.ui.theme.JajanManiaTheme
 
 @Composable
@@ -39,8 +38,7 @@ fun BaseOutlinedButton(
     ) {
         Text(
             text = text,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.labelLarge,
             textAlign = TextAlign.Center,
         )
     }
