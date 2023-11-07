db11b1a14928430239bab054c297661eb82d4108
Taufik Dito Wijaya <taufikdito1@gmail.com>
2023-11-07 22:15:01 +0700

    feat(vendor): revamp ui detail transaction

--- vendor/transaction/src/main/java/com/tokodizital/jajanmania/vendor/transaction/detail/DetailTransactionScreen.kt
+++ vendor/transaction/src/main/java/com/tokodizital/jajanmania/vendor/transaction/detail/DetailTransactionScreen.kt
@@ -1,14 +1,14 @@
 package com.tokodizital.jajanmania.vendor.transaction.detail
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,11 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tokodizital.jajanmania.core.domain.model.Jajan
 import com.tokodizital.jajanmania.ui.components.appbars.DetailTopAppBar
-import com.tokodizital.jajanmania.ui.components.buttons.BaseButton
+import com.tokodizital.jajanmania.ui.components.customer.CustomerJajanTransactionItem
+import com.tokodizital.jajanmania.ui.components.customer.CustomerTotalTransactionFooter
 import com.tokodizital.jajanmania.ui.theme.JajanManiaTheme
 import com.tokodizital.jajanmania.ui.theme.Typography
 
@@ -35,12 +36,27 @@ fun DetailTransactionScreen(
     onNavigationClicked: () -> Unit = {}
 ) {
 
+    val dummyJajan = listOf(
+        Jajan(1, 1, "Batagor Isi 5", "Gorengan", 10000L, ""),
+        Jajan(3, 1, "Batagor Isi 10", "Gorengan", 18000L, ""),
+        Jajan(2, 1, "Batagor Isi 7", "Gorengan", 14000L, "")
+    )
+    val dummyCount = 2
+    var transactionItems by remember {
+        mutableStateOf(
+            dummyJajan
+        )
+    }
+    var total = 0
+    for(item in transactionItems) {
+        total += (item.price.toInt() * dummyCount)
+    }
+
     var transactionPaymentMethod by remember { mutableStateOf("Non-Tunai") }
-    var transactionStatus by remember { mutableStateOf("Menunggu Pembayaran") }
     var transactionTime by remember { mutableStateOf("15:33:45") }
     var transactionDate by remember { mutableStateOf("3 Oktober 2023") }
     var transactionIdentifier by remember { mutableStateOf("776F345CE-5") }
-    var transactionTotal by remember { mutableStateOf("Rp.58.000") }
+    var transactionTotal by remember { mutableStateOf(total )}
 
     Scaffold(
         topBar = {
@@ -51,82 +67,55 @@ fun DetailTransactionScreen(
         },
         modifier = modifier
     ) { paddingValues ->
-        Column(
-            modifier = Modifier
+        Box(modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
                 .padding(paddingValues),
         ) {
-            Column(
+            LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
-                DetailTransactionHeader(
-                    photo = "",
-                    transactionCode = 12345665)
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Text(text = "Rincian Transaksi", style = Typography.titleMedium)
-                Spacer(modifier = Modifier.size(16.dp))
-                Column{
-                    DetailTransactionBody(
-                        tittle = "Metode Pembayaran",
-                        body = transactionPaymentMethod
-                    )
-                    DetailTransactionBody(
-                        tittle = "Status",
-                        body = transactionStatus
-                    )
-                    DetailTransactionBody(
-                        tittle = "Waktu",
-                        body = transactionTime
-                    )
-                    DetailTransactionBody(
-                        tittle = "Tanggal",
-                        body = transactionDate
-                    )
-                    DetailTransactionBody(
-                        tittle = "ID Transaksi",
-                        body = transactionIdentifier
-                    )
-                    Spacer(modifier = Modifier.size(16.dp))
-                    DetailTransactionBody(
-                        tittle = "Total",
-                        body = transactionTotal
-                    )
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Rincian Transaksi", style = Typography.titleMedium)
+                        Spacer(modifier = Modifier.size(16.dp))
+                        Column{
+                            DetailTransactionBody(
+                                tittle = "Metode Pembayaran",
+                                body = transactionPaymentMethod
+                            )
+                            Spacer(modifier = Modifier.size(8.dp))
+                            DetailTransactionBody(
+                                tittle = "Waktu",
+                                body = transactionTime
+                            )
+                            Spacer(modifier = Modifier.size(8.dp))
+                            DetailTransactionBody(
+                                tittle = "Tanggal",
+                                body = transactionDate
+                            )
+                            Spacer(modifier = Modifier.size(8.dp))
+                            DetailTransactionBody(
+                                tittle = "ID Transaksi",
+                                body = transactionIdentifier
+                            )
+                            Spacer(modifier = Modifier.size(16.dp))
+                            Text(text = "Rincian", style = Typography.titleMedium)
+                        }
+                    }
+                }
+                items(items = transactionItems, key = {it.id}) {
+                    CustomerJajanTransactionItem(jajan = it, count = dummyCount)
+                }
+                item {
+                    CustomerTotalTransactionFooter(totalPrice = transactionTotal )
                 }
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp, end = 16.dp),
-            ) {
-                BaseButton(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(end = 8.dp),
-                    text = "Batalkan",
-                    containerColor = Color.Red
-                )
-                BaseButton(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 8.dp),
-                    text = "Bayar Tunai"
-                )
-            }
-            BaseButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp, end = 16.dp),
-                text = "Tutup"
-            )
         }
     }
 }
